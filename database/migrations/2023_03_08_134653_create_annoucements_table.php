af20aa8fd649cc740b9d96f7b307bd2d2daa6c48
Max <maxfergana1986@gmail.com>
2023-03-08 23:32:25 +0500

    08032023

---
+++ database/migrations/2023_03_08_134653_create_annoucements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('annoucements', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category');
+            $table->string('slug',50);
+            $table->string('tags',200);
+            $table->string('title',150);
+            $table->text('body');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('annoucements');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Annoucement;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Annoucement::factory(10)->create();
+        Category::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
